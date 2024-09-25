36ca6ff2e69214e330f3094f3a03976211365adc
11azeem <11azeemuddin@gmail.com>
2024-09-25 13:46:05 +0530

    Initial commit

---
+++ src/test/java/com/hms/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hms.customerservice;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
